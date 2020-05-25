09f8062a61805ddf167cce6d22456f76f75d67a3
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-05-26 00:12:43 +0100

    Feat: completed weather app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue';
+export default {
+  name: 'app',
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  overflow-y: auto;
+}
+
 #nav {
-  padding: 30px;
+  padding: 10px;
+  text-align: center;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+  text-decoration: underline;
 }
 </style>
